09694ebee4923ae73ca5b926bda966c03a9305c2
Nishanth Shanmugham <nishanths@utexas.edu>
2020-10-11 22:09:51 +0530

    remove doc comment for cmd/exhaustive, prefer to use README

--- cmd/exhaustive/exhaustive.go
+++ cmd/exhaustive/exhaustive.go
@@ -2,21 +2,6 @@
 //
 // For details see the doc comment for the exhaustive package that this command
 // is based upon at https://godoc.org/github.com/nishanths/exhaustive.
-//
-// Usage
-//
-//
-//   Usage: exhaustive [-flags] [packages...]
-//
-//   Flags:
-//     -default-signifies-exhaustive
-//       	switch statements are considered exhaustive if a 'default' case is present
-//     -fix
-//      	apply all suggested fixes
-//
-//   Examples:
-//     exhaustive code.org/proj/...
-//     exhaustive -fix example.org/foo/pkg example.org/foo/bar
 package main
 
 import (
