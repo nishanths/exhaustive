d254f6e2dac37dc2675aa27516b795f6e5adc87e
Nishanth Shanmugham <nishanths@utexas.edu>
2020-10-13 02:24:19 +0530

    add gob encode/decode test for fact types

--- exhaustive_test.go
+++ exhaustive_test.go
@@ -1,6 +1,9 @@
 package exhaustive
 
 import (
+	"bytes"
+	"encoding/gob"
+	"reflect"
 	"testing"
 
 	"golang.org/x/tools/go/analysis/analysistest"
@@ -16,4 +19,30 @@ func TestSwitch(t *testing.T) {
 
 func TestSwitchFix(t *testing.T) {
 	analysistest.RunWithSuggestedFixes(t, analysistest.TestData(), Analyzer, "switchfix/...")
+}
+
+func TestGobCompatible(t *testing.T) {
+	// The analysis package does this internally, but we need to do it
+	// manually here for the test.
+	for _, typ := range Analyzer.FactTypes {
+		gob.Register(typ)
+	}
+
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	dec := gob.NewDecoder(&buf)
+
+	for _, typ := range Analyzer.FactTypes {
+		t.Run(reflect.TypeOf(typ).String(), func(t *testing.T) {
+			buf.Reset()
+			if err := enc.Encode(typ); err != nil {
+				t.Errorf("failed to encode: %s", err)
+				return
+			}
+			if err := dec.Decode(typ); err != nil {
+				t.Errorf("failed to decode: %s", err)
+				return
+			}
+		})
+	}
 }
