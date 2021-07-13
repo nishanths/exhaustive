22d06b47dfebe3665b32e54d186b747ad9da99eb
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2021-07-14 05:16:29 +0530

    more

--- fact_test.go
+++ fact_test.go
@@ -1,1 +1,15 @@
 package exhaustive
+
+import "testing"
+
+func TestEnumsFact(t *testing.T) {
+	t.Run("String()", func(t *testing.T) {
+		e := enumsFact{
+			Enums: map[string]*enumMembers{
+				"Biome": {OrderedNames: []string{"Tundra", "Savanna", "Desert"}},
+				"op":    {OrderedNames: []string{"add", "sub", "mul", "quotient", "remainder"}},
+			},
+		}
+		assertEqual(t, "Biome:Tundra,Savanna,Desert; op:add,sub,mul,quotient,remainder", e.String())
+	})
+}
--- testutil_test.go
+++ testutil_test.go
@@ -3,23 +3,29 @@
 package exhaustive
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func ptrString(s string) *string { return &s }
 
-func assertEqual(t *testing.T, want, got interface{}) bool {
+func assertEqualf(t *testing.T, want, got interface{}, format string, args ...interface{}) bool {
 	t.Helper()
 	if !reflect.DeepEqual(want, got) {
-		t.Errorf("want %v, got %v", want, got)
+		t.Errorf("%swant %v, got %v", logPrefix(format, args...), want, got)
 		return false
 	}
 	return true
 }
 
+func assertEqual(t *testing.T, want, got interface{}) bool {
+	t.Helper()
+	return assertEqualf(t, want, got, "")
+}
+
 // v's underlying type must not be an interface type. (in other words,
-// calls to assertNil must pass a concrete pointer)
+// calls to assertNil must pass a concrete pointer.)
 func assertNil(t *testing.T, v interface{}) bool {
 	t.Helper()
 	if isNil(t) {
@@ -31,9 +37,6 @@ func assertNil(t *testing.T, v interface{}) bool {
 
 // poor man's copy of https://github.com/stretchr/testify/blob/acba37e5db06f0093b465a7d47822bf13644b66c/assert/assertions.go#L520
 func isNil(object interface{}) bool {
-	if object == nil {
-		return true
-	}
 	value := reflect.ValueOf(object)
 	return value.IsNil()
 }
@@ -54,4 +57,11 @@ func assertError(t *testing.T, err error) bool {
 		return false
 	}
 	return true
+}
+
+func logPrefix(format string, args ...interface{}) string {
+	if format != "" {
+		return fmt.Sprintf(format, args...) + ": "
+	}
+	return ""
 }
