c2990543317d1db5ddb30c9561972b215aa27f21
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2021-08-12 00:24:16 +0530

    refactor test

--- enum_test.go
+++ enum_test.go
@@ -8,30 +8,34 @@ import (
 )
 
 func TestEnumMembers_add(t *testing.T) {
-	var v enumMembers
-	v.add("foo", nil)
-	v.add("z", ptrString("X"))
-	v.add("bar", nil)
-	v.add("y", ptrString("Y"))
-	v.add("x", ptrString("X"))
+	t.Run("basic", func(t *testing.T) {
+		var v enumMembers
+		v.add("foo", nil)
+		v.add("z", ptrString("X"))
+		v.add("bar", nil)
+		v.add("y", ptrString("Y"))
+		v.add("x", ptrString("X"))
 
-	if want, got := []string{"foo", "z", "bar", "y", "x"}, v.OrderedNames; !reflect.DeepEqual(want, got) {
-		t.Errorf("want %v, got %v", want, got)
-	}
-	if want, got := map[string]string{
-		"z": "X",
-		"y": "Y",
-		"x": "X",
-	}, v.NameToValue; !reflect.DeepEqual(want, got) {
-		t.Errorf("want %v, got %v", want, got)
-	}
+		if want, got := []string{"foo", "z", "bar", "y", "x"}, v.OrderedNames; !reflect.DeepEqual(want, got) {
+			t.Errorf("want %v, got %v", want, got)
+		}
+		if want, got := map[string]string{
+			"z": "X",
+			"y": "Y",
+			"x": "X",
+		}, v.NameToValue; !reflect.DeepEqual(want, got) {
+			t.Errorf("want %v, got %v", want, got)
+		}
 
-	if want, got := map[string][]string{
-		"X": {"z", "x"},
-		"Y": {"y"},
-	}, v.ValueToNames; !reflect.DeepEqual(want, got) {
-		t.Errorf("want %v, got %v", want, got)
-	}
+		if want, got := map[string][]string{
+			"X": {"z", "x"},
+			"Y": {"y"},
+		}, v.ValueToNames; !reflect.DeepEqual(want, got) {
+			t.Errorf("want %v, got %v", want, got)
+		}
+	})
+
+	// TODO: add tests for iota, repeated values, ...
 }
 
 var enumpkg = func() *packages.Package {
