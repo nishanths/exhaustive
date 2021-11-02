cd8f90f3bc57caf43041621f499548d9fa588bc3
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2021-11-03 02:42:46 +0530

    test parenthesized switch tag

--- testdata/src/general/x/general.go
+++ testdata/src/general/x/general.go
@@ -61,6 +61,13 @@ func _k(d Direction) {
 	case (E):
 	case (W):
 	}
+
+	// Parenthesized values in switch tag.
+	switch (d) { // want "^missing cases in switch of type Direction: S, directionInvalid$"
+	case N:
+	case E:
+	case W:
+	}
 }
 
 func _f() {
