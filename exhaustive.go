204a169739fcf4275b645aed228dd487fca841f9
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2021-11-07 20:54:14 +0530

    .

--- exhaustive.go
+++ exhaustive.go
@@ -17,10 +17,10 @@ const (
 	IgnoreEnumMembersFlag          = "ignore-enum-members"
 	PackageScopeOnly               = "package-scope-only"
 
+	typealiasFlag = "typealias"
+
 	IgnorePatternFlag    = "ignore-pattern"    // Deprecated: see IgnoreEnumMembersFlag instead.
 	CheckingStrategyFlag = "checking-strategy" // Deprecated.
-
-	typealiasFlag = "typealias"
 )
 
 var (
@@ -29,9 +29,6 @@ var (
 	fDefaultSignifiesExhaustive bool
 	fIgnoreEnumMembers          regexpFlag
 	fPackageScopeOnly           bool
-
-	// Derprecated flags.
-	fDeprecated string
 
 	// Internal flags.
 	fTypealias bool
@@ -46,26 +43,25 @@ func resetFlags() {
 	fIgnoreEnumMembers = regexpFlag{}
 	fPackageScopeOnly = false
 
-	// Deprecated flags.
-	fDeprecated = ""
-
 	// Internal flags.
 	fTypealias = true
 }
 
 func init() {
+	var deprecated string
+
 	// Public flags.
 	Analyzer.Flags.BoolVar(&fCheckGeneratedFiles, CheckGeneratedFlag, false, "check switch statements in generated files")
 	Analyzer.Flags.BoolVar(&fDefaultSignifiesExhaustive, DefaultSignifiesExhaustiveFlag, false, "presence of \"default\" case in switch statements satisfies exhaustiveness, even if all enum members are not listed")
 	Analyzer.Flags.Var(&fIgnoreEnumMembers, IgnoreEnumMembersFlag, "enum members matching `regex` do not have to be listed in switch statements to satisfy exhaustiveness")
 	Analyzer.Flags.BoolVar(&fPackageScopeOnly, PackageScopeOnly, false, "consider enums only in package scopes, not in inner scopes")
 
-	// Deprecated flags.
-	Analyzer.Flags.StringVar(&fDeprecated, IgnorePatternFlag, "", "no effect (deprecated); see -"+IgnoreEnumMembersFlag+" instead")
-	Analyzer.Flags.StringVar(&fDeprecated, CheckingStrategyFlag, "", "no effect (deprecated)")
-
 	// Internal flags.
 	Analyzer.Flags.BoolVar(&fTypealias, typealiasFlag, true, "handle type alias enums")
+
+	// Deprecated flags.
+	Analyzer.Flags.StringVar(&deprecated, IgnorePatternFlag, "", "no effect (deprecated); see -"+IgnoreEnumMembersFlag+" instead")
+	Analyzer.Flags.StringVar(&deprecated, CheckingStrategyFlag, "", "no effect (deprecated)")
 }
 
 var Analyzer = &analysis.Analyzer{
