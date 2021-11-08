2e6837ef2cfa37bf26fce0933f60b7da2f5e8619
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2021-11-08 23:02:20 +0530

    testdata typealias

---
+++ testdata/src/enum/.typealias/typealias.go
@@ -0,0 +1,19 @@
+package enum
+
+import (
+	"enum/otherpkg"
+)
+
+// type T2 int
+
+// type T1 = T2
+
+// const (
+// 	T1_A T1 = iota
+// 	T1_B
+// )
+
+type T3 = otherpkg.T4
+type T5 = otherpkg.T5
+
+// type T6 = int
