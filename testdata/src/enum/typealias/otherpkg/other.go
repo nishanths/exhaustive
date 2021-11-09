d897d24690238330ee6409403c3fecec0d85b551
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2021-11-09 08:06:55 +0530

    finish type alias tests

--- testdata/src/enum/typealias/otherpkg/other.go
+++ testdata/src/enum/typealias/otherpkg/other.go
@@ -1,15 +1,16 @@
 package otherpkg
 
-import "enum/typealias/otherpkg/anotherpkg"
+import "enum/typealias/anotherpkg"
 
 type T5 int
 type T7 = T5
 
-type T11 int // want T11:"^T11_A,T11_B$"
+type T11 int // want T11:"^T11_A,T11_B,T11_C$"
 
 const (
 	T11_A T11 = iota
 	T11_B
+	T11_C
 )
 
 type T16 = T11
