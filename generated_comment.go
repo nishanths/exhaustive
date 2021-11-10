296a1a5b6539b33ca6888a1c7944a2f674b790ab
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2021-11-10 10:14:21 +0530

    fix generated code checks: comment should appear early in file (#29)
    Account for this requirement:
    
    This line must appear before the first non-comment, non-blank
    text in the file.
    
    http://golang.org/s/generatedcode

--- generated_comment.go
+++ generated_comment.go
@@ -5,16 +5,35 @@ import (
 	"regexp"
 )
 
-// Adapted from https://gotools.org/dmitri.shuralyov.com/go/generated
+// Spec
+// http://golang.org/s/generatedcode
+//
+// To convey to humans and machine tools that code is generated, generated
+// source should have a line that matches the following regular expression (in
+// Go syntax):
+//
+//   ^// Code generated .* DO NOT EDIT\.$
+//
+// This line must appear before the first non-comment, non-blank
+// text in the file.
 
 func isGeneratedFile(file *ast.File) bool {
+	// NOTE: file.Comments includes file.Doc as well, so no need
+	// to separately check file.Doc.
+
 	for _, c := range file.Comments {
 		for _, cc := range c.List {
-			s := cc.Text // "\n" already removed (see doc comment)
-			if len(s) >= 1 && s[len(s)-1] == '\r' {
-				s = s[:len(s)-1] // Trim "\r".
+			// This check is intended to handle "must appear before the
+			// first non-comment, non-blank text in the file".
+			// TODO: Is this check fully correct?
+			if c.Pos() > file.Package {
+				return false
 			}
-			if isGeneratedFileComment(s) {
+			// According to the docs:
+			// '\r' has been removed.
+			// '\n' has been removed for //-style comments, which is what we care about.
+			// Also manually verified.
+			if isGeneratedFileComment(cc.Text) {
 				return true
 			}
 		}
