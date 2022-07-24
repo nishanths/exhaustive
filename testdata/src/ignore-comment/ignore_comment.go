7935df20ed43746fd2fea0c4b6dd79d0d8661060
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2022-07-24 05:57:58 +0530

    add more tests for ignore-comment (#47)

--- testdata/src/ignore-comment/ignore_comment.go
+++ testdata/src/ignore-comment/ignore_comment.go
@@ -27,13 +27,84 @@ func _b() {
 	var d Direction
 
 	// this should not report.
-	//exhaustive:ignore
+	//exhaustive:ignore ... more arbitrary comment content (e.g. an explanation) ...
 	switch d {
 	case N:
 	case S:
 	case W:
 	default:
 	}
+
+	// this should report.
+	switch d { // want "^missing cases in switch of type Direction: E, directionInvalid$"
+	case N:
+	case S:
+	case W:
+	default:
+	}
+}
+
+func _c0() {
+	var d Direction
+
+	// this should report: according to go/ast, the comment is not considered to
+	// be associated with the switch statement node.
+	switch d { //exhaustive:ignore // want "^missing cases in switch of type Direction: E, directionInvalid$"
+	case N:
+	case S:
+	case W:
+	default:
+	}
+
+	// this should report.
+	switch d { // want "^missing cases in switch of type Direction: E, directionInvalid$"
+	case N:
+	case S:
+	case W:
+	default:
+	}
+}
+
+func _c1() {
+	var d Direction
+
+	// this should report: according to go/ast, the comment is not considered to
+	// be associated with the switch statement node.
+	switch d { // want "^missing cases in switch of type Direction: E, directionInvalid$"
+	//exhaustive:ignore
+	case N:
+	case S:
+	case W:
+	default:
+	}
+
+	// this should report.
+	switch d { // want "^missing cases in switch of type Direction: E, directionInvalid$"
+	case N:
+	case S:
+	case W:
+	default:
+	}
+}
+
+func _d() {
+	// this should report.
+	switch (func() Direction { // want "^missing cases in switch of type Direction: E, directionInvalid$"
+		// this should not report.
+		var x Direction
+		//exhaustive:ignore
+		switch x {
+		case N, S:
+		}
+		return N
+	})() {
+	case N:
+	case S:
+	case W:
+	default:
+	}
+
+	var d Direction
 
 	// this should report.
 	switch d { // want "^missing cases in switch of type Direction: E, directionInvalid$"
