c28f97f46eb5f737d9ca9ce3fdc107ff3f64414b
Nishanth Shanmugham <bambooparch@gmail.com>
2022-11-26 21:13:43 +0530

    separate out test files

--- exhaustive_test.go
+++ exhaustive_test.go
@@ -7,69 +7,68 @@ import (
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
-func TestExhaustive(t *testing.T) {
-	run := func(t *testing.T, pattern string, setup ...func()) {
-		t.Helper()
-		t.Run(pattern, func(t *testing.T) {
-			resetFlags()
-			// default to checking switch and map for test.
-			fCheck = stringsFlag{
-				[]string{
-					string(elementSwitch),
-					string(elementMap),
-				},
-				nil,
-			}
-			for _, f := range setup {
-				f()
-			}
-			analysistest.Run(t, analysistest.TestData(), Analyzer, pattern)
-		})
-	}
+func runTest(t *testing.T, pattern string, setup ...func()) {
+	t.Helper()
+	t.Run(pattern, func(t *testing.T) {
+		resetFlags()
+		// default to checking switch and map for test.
+		fCheck = stringsFlag{
+			[]string{
+				string(elementSwitch),
+				string(elementMap),
+			},
+			nil,
+		}
+		for _, f := range setup {
+			f()
+		}
+		analysistest.Run(t, analysistest.TestData(), Analyzer, pattern)
+	})
+}
 
+func TestExhaustive(t *testing.T) {
 	// Enum discovery, enum types.
-	run(t, "enum/...")
+	runTest(t, "enum/...")
 
 	// Tests for the -check-generated flag.
-	run(t, "generated-file/check-generated-off/...")
-	run(t, "generated-file/check-generated-on/...", func() { fCheckGenerated = true })
+	runTest(t, "generated-file/check-generated-off/...")
+	runTest(t, "generated-file/check-generated-on/...", func() { fCheckGenerated = true })
 
 	// Tests for the -default-signifies-exhaustive flag.
 	// (For tests with this flag off, see other testdata packages
 	// such as "general/...".)
-	run(t, "default-signifies-exhaustive/default-absent/...", func() { fDefaultSignifiesExhaustive = true })
-	run(t, "default-signifies-exhaustive/default-present/...", func() { fDefaultSignifiesExhaustive = true })
+	runTest(t, "default-signifies-exhaustive/default-absent/...", func() { fDefaultSignifiesExhaustive = true })
+	runTest(t, "default-signifies-exhaustive/default-present/...", func() { fDefaultSignifiesExhaustive = true })
 
 	// Tests for the -ignore-enum-member flag.
-	run(t, "ignore-enum-member/...", func() {
+	runTest(t, "ignore-enum-member/...", func() {
 		re := regexp.MustCompile(`_UNSPECIFIED$|^general/y\.Echinodermata$|^ignore-enum-member.User$`)
 		fIgnoreEnumMembers = regexpFlag{re}
 	})
 
 	// Tests for -package-scope-only flag.
-	run(t, "scope/allscope/...")
-	run(t, "scope/pkgscope/...", func() { fPackageScopeOnly = true })
+	runTest(t, "scope/allscope/...")
+	runTest(t, "scope/pkgscope/...", func() { fPackageScopeOnly = true })
 
 	// Program elements with ignore comment should not be
 	// checked during implicitly exhaustive mode.
-	run(t, "ignore-comment/...")
+	runTest(t, "ignore-comment/...")
 
 	// Program elements without enforce comment should not be
 	// checked in explicitly exhaustive mode.
-	run(t, "enforce-comment/...", func() {
+	runTest(t, "enforce-comment/...", func() {
 		fExplicitExhaustiveSwitch = true
 		fExplicitExhaustiveMap = true
 	})
 
 	// To satisfy exhaustiveness, it is sufficient for each unique constant
 	// value of the members to be listed, not each member by name.
-	run(t, "duplicate-enum-value/...")
+	runTest(t, "duplicate-enum-value/...")
 
-	run(t, "typealias/...")
-	run(t, "typeparam/...")
+	runTest(t, "typealias/...")
 
 	// mixture of general tests.
-	run(t, "general/...")
+	runTest(t, "general/...")
 }
 
 func assertNoError(t *testing.T, err error) {
