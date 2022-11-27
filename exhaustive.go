746c9727dc27095a24ada617680b89bfa2be288f
Nishanth Shanmugham <bambooparch@gmail.com>
2022-11-27 17:37:25 +0530

    add full import path example in package doc

--- exhaustive.go
+++ exhaustive.go
@@ -194,7 +194,7 @@ To ignore specific constants in exhaustiveness checks, use the
 -ignore-enum-members flag. Similarly, to ignore specific types, use the
 -ignore-enum-types flag. For example:
 
-	exhaustive -ignore-enum-types '^reflect\.Kind$|^time\.Duration$'
+	exhaustive -ignore-enum-types '^time\.Duration$|^example.org/measure\.Unit$'
 
 [language spec]: https://golang.org/ref/spec
 */
