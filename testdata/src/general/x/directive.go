499a89c77865de0c96e8d06e6e4690d8a5f2677d
Nishanth Shanmugham <bambooparch@gmail.com>
2024-02-27 21:19:43 +0000

    add testdata for no-op directive comment

---
+++ testdata/src/general/x/directive.go
@@ -0,0 +1,28 @@
+package x
+
+func _() {
+	var d Direction
+
+	// This comment should not produce an diagnostic (note unknown prefix "exhauster:"
+	//instead of "exhaustive:".
+	//exhauster:foo
+	switch d {
+	case N:
+	case S:
+	case W:
+	case E:
+	case directionInvalid:
+	default:
+	}
+
+	// This comment should not produce an diagnostic (note unknown prefix "exhauster:"
+	//instead of "exhaustive:".
+	//exhauster:foo
+	_ = map[Direction]int{
+		N:                1,
+		S:                2,
+		W:                3,
+		E:                4,
+		directionInvalid: 0,
+	}
+}
